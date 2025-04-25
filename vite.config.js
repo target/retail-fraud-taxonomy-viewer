c9c97406b9274752851fb9683276b30f9f6e310c
z085941 <Yashwantha.BN@target.com>
2025-04-25 08:08:06 +0530

    Fixed the Dev deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   build: {
-    outDir: process.env.BUILD_MODE === 'development' ? 'dist/dev1' : 'dist', // If development, output to dist/dev
+    outDir: process.env.BUILD_MODE === 'development' ? 'dist/dev' : 'dist', // If development, output to dist/dev
   },
   base: process.env.BUILD_MODE === 'development'
     ? '/retail-fraud-taxonomy-viewer/dev1/'
